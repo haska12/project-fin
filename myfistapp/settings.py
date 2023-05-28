cfc3ae4214c591257a02f91df47210f25444760f
haska12 <88626706+haska12@users.noreply.github.com>
2023-05-28 10:23:33 +0100

    cv
    static file fix

--- myfistapp/settings.py
+++ myfistapp/settings.py
@@ -128,6 +128,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+ 
+
+STATIC_ROOT=os.path.join(BASE_DIR,"/static/")#correct
+
+
+
+#Add this in your settings.py file:
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
