d3ff0a4d6352cf58426f5c066a1c8676714c42d5
haska12 <88626706+haska12@users.noreply.github.com>
2023-06-25 18:30:58 +0100

    t

--- myfistapp/settings.py
+++ myfistapp/settings.py
@@ -160,7 +160,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
