9cac544d57aaa1f7140286786740f79f9db5c204
haska12 <88626706+haska12@users.noreply.github.com>
2023-06-25 18:23:10 +0100

    Update settings.py

--- myfistapp/settings.py
+++ myfistapp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gnvp^m6=7&#b^5j%s7oia_0c!r%r0-p@)g3sbm%@#_jfap_7q9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
