a241af74e11d07978edc4cea57ec2d6ccc844740
haska12 <88626706+haska12@users.noreply.github.com>
2023-06-25 15:12:48 +0100

    s3

--- myfistapp/settings.py
+++ myfistapp/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gnvp^m6=7&#b^5j%s7oia_0c!r%r0-p@)g3sbm%@#_jfap_7q9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
