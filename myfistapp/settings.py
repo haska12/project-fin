b3ffe4115542e273886147b81005836e035a6382
haska12 <88626706+haska12@users.noreply.github.com>
2023-06-25 18:08:29 +0100

    h

--- myfistapp/settings.py
+++ myfistapp/settings.py
@@ -153,6 +153,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT=os.path.join(BASE_DIR,"/static/")#correct
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 #Add this in your settings.py file:
